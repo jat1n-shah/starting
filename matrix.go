0e0ee132a68119e61c462da32c1d1f70727017b1
jat1n-shah <jatinshah.50.js@gmail.com>
2018-11-29 22:02:55 +0530

    changes in matrix

--- matrix.go
+++ matrix.go
@@ -29,12 +29,12 @@ func main() {
 		}
 	}
 
-	fmt.Println("Subtraction of two matrix = ")
+	/*fmt.Println("Subtraction of two matrix = ")
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 2; j++ {
 			c[i][j] = a[i][j] - b[i][j]
 			fmt.Println(c)
 		}
-	}
+	}*/
 
 }
